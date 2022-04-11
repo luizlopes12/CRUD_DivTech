8c71246ac888e74f1649c07a388914597828b702
Luiz Lopes <luizlopes12@outlook.com>
2022-04-11 13:37:09 -0300

    Update update.php

--- crud/update.php
+++ crud/update.php
@@ -4,9 +4,9 @@ include "./connect.php";
 $idGet = $_POST['id'];
 $nome = $_POST['nome'];
 $cnpj = $_POST['cnpj'];
-$especializacao = $_POST['especializacao']
+$especialidade = $_POST['especialidade'];
 //Query para atualizar
-mysqli_query($connect,"update fornecedores set nome = '$nome', cnpj = '$cnpj', espeializacao = '$especializacao' where id = $idGet");
+mysqli_query($connect,"update fornecedores set nome = '$nome', cnpj = '$cnpj', especialidade = '$especialidade' where id = $idGet");
 
 //Pagina de destino depois da execução query
 header("location:../index.php");
